cc5cd21833b3964cc23a349d80d7522322296c71
lati111 <jvzelst111@gmail.com>
2022-11-15 09:11:31 +0100

    Added make noise to animals

--- make_noise_classes.php
+++ make_noise_classes.php
@@ -1,29 +1,7 @@
 <?php
     require_once ('make_noise_interface.php');
 
-    // TODO: make the class Dog implement the IMakeNoise interface.
-    class Dog // implements ...
-    {
-        private $name;
-
-        public function __construct(string $name)
-        {
-            $this->name = $name;
-        }
-
-        public function getName() {
-            return $this->name;
-        }
-
-        public function makeNoise() : string // HOE de hond het geluid maakt.
-        {
-            // TODO: Implement makeNoise() method.
-
-        }
-    }
-
-    // TODO: make the class Fish implement the IMakeNoise interface.
-    class Fish // implements ...
+    class Dog implements IMakeNoise
     {
         private $name;
 
@@ -38,13 +16,11 @@
 
         public function makeNoise() : string
         {
-            // TODO: Implement makeNoise() method.
-
+            return "loud barking";
         }
     }
 
-    // TODO: make the class Bird implement the IMakeNoise interface.
-    class Bird // implements ...
+    class Fish implements IMakeNoise
     {
         private $name;
 
@@ -59,8 +35,26 @@
 
         public function makeNoise() : string
         {
-            // TODO: Implement makeNoise() method.
+            return "bubbling fish noises";
 
         }
     }
-?>
+
+    class Bird implements IMakeNoise
+    {
+        private $name;
+
+        public function __construct(string $name)
+        {
+            $this->name = $name;
+        }
+
+        public function getName() {
+            return $this->name;
+        }
+
+        public function makeNoise() : string
+        {
+            return "annoying bird noises";
+        }
+    }
